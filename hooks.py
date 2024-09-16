cb0ab326efc2f107f32b14acc86067a23c4eb027
LucasCordova <lpcordova@outlook.com>
2024-09-15 17:27:52 -0700

    Deployed 1cf69e7 with MkDocs version: 1.5.3

--- hooks.py
+++ hooks.py
@@ -3,5 +3,5 @@ import shutil
 
 def copy_get(config, **kwargs):
     site_dir = config['site_dir']
-    shutil.copy('static/iarray.py', os.path.join(site_dir, 'iarray.py'))
-    shutil.copy('static/array.py', os.path.join(site_dir, 'array.py'))
+    shutil.copy('datastructures/iarray.py', os.path.join(site_dir, 'iarray.py'))
+    shutil.copy('datastructures/array.py', os.path.join(site_dir, 'array.py'))
