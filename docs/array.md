32bd85f9c503a3fbf39dcb3cb9e235d7e9137c5c
LucasCordova <lpcordova@outlook.com>
2024-09-17 10:47:32 -0700

    Updated array test file

--- docs/array.md
+++ docs/array.md
@@ -10,6 +10,7 @@
 
 - [IArray Interface Python file](../iarray.py)
 - [Array Class Python file](../array.py)
+- [Array Test Python file](../test_array.py)
 
 ## Specification
 
--- docs/hooks.py
+++ docs/hooks.py
@@ -5,3 +5,4 @@ def copy_get(config, **kwargs):
     site_dir = config['site_dir']
     shutil.copy('datastructures/iarray.py', os.path.join(site_dir, 'iarray.py'))
     shutil.copy('datastructures/array.py', os.path.join(site_dir, 'array.py'))
+    shutil.copy('static/test_array.py', os.path.join(site_dir, 'test_array.py'))
