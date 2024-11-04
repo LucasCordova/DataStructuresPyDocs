f743f670693fe6be6645b042afbc99375f321307
LucasCordova <lpcordova@outlook.com>
2024-11-03 20:17:25 -0800

    Deployed 2cf25a3 with MkDocs version: 1.6.1

--- hooks.py
+++ hooks.py
@@ -17,6 +17,7 @@ def copy_get(config, **kwargs):
     shutil.copy('datastructures/ilinkedlist.py', os.path.join(site_dir, 'ilinkedlist.py'))
     shutil.copy('datastructures/linkedlist.py', os.path.join(site_dir, 'linkedlist.py'))
     shutil.copy('datastructures/test_linkedlist.py', os.path.join(site_dir, 'test_linkedlist.py'))
+    shutil.copy('static/graphs/css/graphs_intro.css', os.path.join(site_dir, 'assets', 'stylesheets', 'graphs_intro.css'))
+    # shutil.copytree('static', os.path.join(site_dir, 'static'))
 
 
-    shutil.copytree('static', os.path.join(site_dir, 'static'))
