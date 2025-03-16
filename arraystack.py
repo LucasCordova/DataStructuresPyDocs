14429ced2ab1de1994411cef10d5f268f4d4cc6c
LucasCordova <lpcordova@outlook.com>
2025-03-16 06:24:43 -0700

    Deployed dccffc5 with MkDocs version: 1.6.1

--- arraystack.py
+++ arraystack.py
@@ -9,6 +9,15 @@ class ArrayStack(IStack[T]):
     
     def __init__(self, max_size: int = 0, data_type=object) -> None:
         ''' Constructor to initialize the stack 
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.empty
+                True
+                >>> s.full
+                False
+                >>> s.maxsize
+                5
         
             Arguments: 
                 max_size: int -- The maximum size of the stack. 
@@ -19,6 +28,20 @@ class ArrayStack(IStack[T]):
     def push(self, item: T) -> None:
         ''' Pushes an item onto the stack.
         
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> s.push(4)
+                >>> s.push(5)
+                >>> s.full
+                True
+                >>> print(repr(s))
+                ArrayStack(5): items: [1, 2, 3, 4, 5]
+                >>> s.push(6)
+                IndexError('Stack is full')
+
             Arguments:
                 item: T -- The item to push onto the stack.
         '''
@@ -26,6 +49,24 @@ class ArrayStack(IStack[T]):
 
     def pop(self) -> T:
         ''' Pops an item from the stack.
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> s.pop()
+                3
+                >>> s.pop()
+                2
+                >>> s.pop()
+                1
+                >>> s.empty
+                True
+                >>> print(repr(s))
+                ArrayStack(5): items: []
+                >>> s.pop()
+                IndexError('Stack is empty')
         
             Returns:
                 T -- The item popped from the stack.
@@ -33,7 +74,19 @@ class ArrayStack(IStack[T]):
         raise NotImplementedError
 
     def clear(self) -> None:
-       ''' Clears the stack. '''
+       ''' Clears the stack. 
+       
+           Examples:
+               >>> s = ArrayStack(max_size=5, data_type=int)
+               >>> s.push(1)
+               >>> s.push(2)
+               >>> s.push(3)
+               >>> s.clear()
+               >>> s.empty
+               True
+               >>> print(repr(s))
+               ArrayStack(5): items: []
+        '''
        raise NotImplementedError
     
     @property
@@ -42,12 +95,39 @@ class ArrayStack(IStack[T]):
         
             Returns:
                 T -- The top item on the stack.
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> s.peek
+                3
+                >>> s.pop()
+                3
+                >>> s.peek
+                2
+                >>> s.pop()
+                2
+                >>> s.peek
+                1
+                >>> s.pop()
+                1
+                >>> s.empty
+                True
+                >>> s.peek
+                IndexError('Stack is empty')
         '''
         raise NotImplementedError
 
     @property
     def maxsize(self) -> int:
         ''' Returns the maximum size of the stack. 
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.maxsize
+                5
         
             Returns:
                 int: The maximum size of the stack.
@@ -56,6 +136,9 @@ class ArrayStack(IStack[T]):
     @property
     def full(self) -> bool:
         ''' Returns True if the stack is full, False otherwise. 
+
+            Examples:
+
         
             Returns:
                 bool: True if the stack is full, False otherwise.
@@ -65,6 +148,18 @@ class ArrayStack(IStack[T]):
     @property
     def empty(self) -> bool:
         ''' Returns True if the stack is empty, False otherwise. 
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.empty
+                True
+                >>> s.push(1)
+                >>> s.empty
+                False
+                >>> s.pop()
+                1
+                >>> s.empty
+                True
         
             Returns:
                 bool: True if the stack is empty, False otherwise.
@@ -73,6 +168,22 @@ class ArrayStack(IStack[T]):
     
     def __eq__(self, other: object) -> bool:
         ''' Compares two stacks for equality.
+
+            Examples:
+                >>> s1 = ArrayStack(max_size=5, data_type=int)
+                >>> s2 = ArrayStack(max_size=5, data_type=int)
+                >>> s1 == s2
+                True
+                >>> s1.push(1)
+                >>> s1 == s2
+                False
+                >>> s2.push(1)
+                >>> s1 == s2
+                True
+                >>> s1.push(2)
+                >>> s2.push(3)
+                >>> s1 == s2
+                False
         
             Arguments:
                 other: object -- The other stack to compare.
@@ -84,6 +195,25 @@ class ArrayStack(IStack[T]):
 
     def __len__(self) -> int:
         ''' Returns the number of items in the stack.
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> len(s)
+                0
+                >>> s.push(1)
+                >>> len(s)
+                1
+                >>> s.push(2)
+                >>> len(s)
+                2
+                >>> s.pop()
+                2
+                >>> len(s)
+                1
+                >>> s.pop()
+                1
+                >>> len(s)
+                0
         
             Returns:
                 int -- The number of items in the stack.
@@ -93,6 +223,22 @@ class ArrayStack(IStack[T]):
     def __contains__(self, item: T) -> bool:
         ''' Returns True if the item is in the stack, False otherwise.
         
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> 1 in s
+                True
+                >>> 2 in s
+                True
+                >>> 3 in s
+                True
+                >>> 4 in s
+                False
+                >>> 5 in s
+                False
+            
             Arguments:
                 item: T -- The item to search for.
                 
@@ -102,9 +248,35 @@ class ArrayStack(IStack[T]):
         raise NotImplementedError
 
     def __str__(self) -> str:
+        ''' Returns a string representation of the stack.
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> print(s)
+                [1, 2, 3]
+        
+            Returns:
+                str -- A string representation of the stack.
+        '''
         return str([self.stack[i] for i in range(self._top)])
     
     def __repr__(self) -> str:
+        ''' Returns a string representation of the stack.
+
+            Examples:
+                >>> s = ArrayStack(max_size=5, data_type=int)
+                >>> s.push(1)
+                >>> s.push(2)
+                >>> s.push(3)
+                >>> repr(s)
+                'ArrayStack(5): items: [1, 2, 3]'
+        
+            Returns:
+                str -- A string representation of the stack.
+        '''
         return f"ArrayStack({self.maxsize}): items: {str(self)}"
     
 if __name__ == '__main__':
